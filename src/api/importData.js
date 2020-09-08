e7d96046823563e9f660d58deb09a36141eda6f6
andreasvogt89 <andreas.vogt89@bluewin.ch>
2020-09-08 14:55:22 +0200

    working on import files

--- src/api/importData.js
+++ src/api/importData.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const serverLog = require('../serverlog/serverlogger');
-const readXlsxFile = require('read-excel-file/node');
 const path = require('path');
-const data = require('../dataHandler/dataHandler');
+const XLSX = require('xlsx');
+const browserObjects = [];
 
 
 router.get('/', (req, res) => {
     serverLog.info("import file from src directory");
-    // File path.
-    readXlsxFile(path.join(__dirname, '../import/source.xlsx')).then((rows) => {
-       let contend = new data(rows);
-       //....
-    })
+
+    let workbook = XLSX.readFile(path.join(__dirname, '../import/source.xlsx'));
+    /* DO SOMETHING WITH workbook HERE */
+    console.log(workbook);
     res.json({
         message: 'Import'
     });
