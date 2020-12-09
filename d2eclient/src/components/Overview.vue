22d1d9326a9fd6c806c2b2d9820d0fb87c6b78f8
andreasvogt89 <30302212+andreasvogt89@users.noreply.github.com>
2020-12-09 20:54:42 +0100

    working on overview(Timeline)

--- d2eclient/src/components/Overview.vue
+++ d2eclient/src/components/Overview.vue
@@ -10,18 +10,17 @@
 
       </v-progress-circular>
     </v-overlay>
-    <h1 class="OVTitle">Overview</h1>
+    <h1 class="OVTitle">Import Timeline</h1>
     <v-btn @click="startImport()" >Import data </v-btn>
      <v-timeline :dense="$vuetify.breakpoint.smAndDown" >
       <v-timeline-item
       v-for="(timeline, i) in timeline"
       :key="i"
       small
-    
       >
       <template v-slot:opposite>
         <span
-          :class="`headline font-weight-bold blue--text`"
+          :class="`headline font-weight-bold white--text`"
           v-text="timeline.value.imported_at"
         ></span>
       </template>
@@ -33,10 +32,18 @@
                 <td>Total imported Objects</td>
                 <td>{{ timeline.value.objectsTotal }}</td>
               </tr>
-              <tr @click="setSheetValue(timeline.value)">
-                <td>Total valid Items</td>
-                <td>{{ timeline.value.validObjects }}</td>
+               <v-tooltip bottom>
+                <template v-slot:activator="{ on, attrs }">
+              <tr @click="setSheetValue(timeline.value)"
+              v-bind="attrs"
+              v-on="on">
+                <td>Total valid Items for OSI PI</td>
+                <td>{{ timeline.value.validObjects }}
+                </td>
               </tr>
+                  </template>
+                <span>Click for details</span>
+              </v-tooltip>
             </tbody>
           </template>
         </v-simple-table>
@@ -46,34 +53,81 @@
     <v-alert v-if="!importState" class="error">No data import yet 😥 ... <br>Ask siemens for help🧠</v-alert>
     <div class="text-center">
     <v-bottom-sheet v-model="sheet" inset>
-      <v-sheet class="text-center" height="500px">
+      <div>
+      <v-sheet 
+      class="text-center blue-grey darken-4"
+      height="auto"
+      rounded
+       >
         <v-btn
           class="mt-6"
           text
           color="error"
           @click="sheet = !sheet"
         >close</v-btn>
-        <div class="my-3">
-            <v-simple-table>
+        <v-card class="ma-2 grey darken-1">
+          <v-card-title>Total per driver</v-card-title>
+          <v-simple-table dark>
           <template v-slot:default >
             <tbody>
               <tr>
-                <td>S7 Items</td>
+                <td>S7 items</td>
                 <td>{{ sheetItem.S7Values }}</td>
               </tr>
               <tr>
-                <td>BACnet Items</td>
+                <td>BACnet items</td>
                 <td>{{ sheetItem.BACValues}}</td>
               </tr>
               <tr>
-                <td>DesigoCC Items</td>
+                <td>DesigoCC items</td>
                 <td>{{ sheetItem.desigoCCValues}}</td>
               </tr>
             </tbody>
           </template>
-        </v-simple-table>
+          </v-simple-table>
+          </v-card>
+          <v-card class="ma-2 grey darken-1">
+          <v-card-title>Total per type</v-card-title>
+          <v-simple-table dark>
+          <template v-slot:default >
+            <tbody>
+              <tr>
+                <td>Analog items</td>
+                <td>{{ sheetItem.analogValues }}</td>
+              </tr>
+              <tr>
+                <td>Binary items</td>
+                <td>{{ sheetItem.binaryValues}}</td>
+              </tr>
+            </tbody>
+          </template>
+          </v-simple-table>
+          </v-card>
+          <div class="my-3">
+          <v-card class="ma-2 grey darken-1">
+           <v-card-title>Objectmodels</v-card-title> 
+          <v-card-title> 
+            <v-text-field
+              v-model="search"
+              append-icon="mdi-magnify"
+              label="Search"
+              single-line
+              hide-details
+            ></v-text-field>
+          </v-card-title>
+          <v-data-table
+            dense
+            dark
+            :headers="headers"
+            :items="sheetItem.objectModels"
+            :search="search"
+            :height="400"
+            >
+          </v-data-table>
+        </v-card>
         </div>
       </v-sheet>
+      </div>
     </v-bottom-sheet>
   </div>
   </v-container>
@@ -93,6 +147,15 @@ export default {
       timeline: [],
       sheet: false,
       sheetItem: {},
+      headers: [{
+              text:"Object model name",
+              align: 'start',
+              value: 'model',
+            },{
+              text:"Number of entries",
+              value: 'amount',
+            }],
+      search: '',
     }
   },
 
@@ -144,7 +207,7 @@ export default {
     setSheetValue(item){
       this.sheet = true;
       this.sheetItem = item;
-    }
+    },
   }
 }
 </script>
@@ -153,15 +216,6 @@ export default {
 .OVTitle{
   font-size: xx-large;
   color: #ffffff;
+  margin: 1em;
 }
-.overview{
-  background: #34495E;
-}
-.boxes{
-  background: #5D6D7E;
-}
-.lastImport {
-  font-weight: bold;
-}
-
 </style>
